964625bfef072b6bb39dd3c413af37b01abb0400
Luiz Henrique <luizhenrique455@gmail.com>
2021-11-09 23:42:20 -0800

    implemented calc and navigator
    -Update calc methods
    -Update screen navigation

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heighLabel: UILabel!
+    
+    var bmiValue = "0.0"
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(Int(sender.value)) + " Kg"
+        
+    }
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heighLabel.text = String(format: "%.2f",  sender.value) + " m"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let weight = weightSlider.value
+        let height = heightSlider.value
+        let bmi = weight / pow(height, 2)
+        bmiValue = String(format: "%.1f", bmi)
+        self.performSegue(withIdentifier: "goToResult", sender: sender)
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = bmiValue
+        }
+    }
+}
+
